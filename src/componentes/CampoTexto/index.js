5f19ec91fa14bfbed58b34691adbc95b42cc8b0d
JoaoDias06 <joaozinhopd06@gmail.com>
2025-04-25 09:23:53 -0300

    componente campo de texto - completo
    componente campo de texto - completo

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,15 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+
+    const placeholdermodificada = `${props.placeholder}...`
+
+    return (
+        <div className='campo-texto'>
+            <label>{props.label}</label>
+            <input placeholder={placeholdermodificada}/>
+        </div>
+    )
+}
+
+export default CampoTexto
